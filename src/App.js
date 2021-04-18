8843ace920972e29a21ee26fee87677154774ca7
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2021-04-18 23:30:55 +0300

    HW

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Profile from "./task1/Profile";
+import user from "./task1/user.json";
+import Statistics from "./task2/Statistics";
+import StatsRender from "./task2/StatsRender";
+import statisticalData from "./task2/statistical-data.json";
+import FriendList from "./task3/FriendList";
+import friends from "./task3/friends.json";
+import TransactionHistory from "./task4/TransactionHistory";
+import transactions from "./task4/transactions.json";
+
+const App = () => {
+    return (
+        <>
+            <Profile {...user}
+            />
+            <Statistics title="Upload stats">
+        <StatsRender stats={statisticalData} />
+            </Statistics>
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </>
+    )
+}
+
+export default App;

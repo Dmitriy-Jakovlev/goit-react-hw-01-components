9fdefda7fcd11afe729db707e04adb024751a576
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2021-08-17 21:15:40 +0300

    исправил  friendList

--- src/task3/FriendList.js
+++ src/task3/FriendList.js
@@ -1,12 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./friendList.module.css";
-import FriendListItem from "./FriendListItem";
+// import FriendListItem from "./FriendListItem";
+import friends from './friends.json'
 
-const FriendList = ({ friends }) => {
+const FriendList = () => {
   return (
     <ul className={styles.friendList}>
-      <FriendListItem friends={friends} />
+      {friends.map(friend => (
+        <li key={friend.id} className={styles.item}>
+          <span
+            className={
+              friend.isOnline ? styles.statusOnline : styles.statusOffline
+            }
+          ></span>
+
+          <img
+            className={styles.avatar}
+            src={friend.avatar}
+            alt="friendAvatar"
+            width="48"
+          />
+          <p className={styles.name}>{friend.name}</p>
+        </li>
+      ))}
     </ul>
   );
 };
